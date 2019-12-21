42486f01cc671cd5d986b1fde1d9942fb62701bd
dannyboy820 <dannyboy.0193@gmail.com>
2019-12-21 16:48:09 +0100

    Extract calculate_prefix_impl

--- erc20/src/contract/prefixedstorage.rs
+++ erc20/src/contract/prefixedstorage.rs
@@ -1,5 +1,17 @@
 use cosmwasm::traits::{ReadonlyStorage, Storage};
 
+// prepend length of the prefix
+fn calculate_prefix_impl(prefix: &[u8]) -> Vec<u8> {
+    if prefix.len() > 0xFF {
+        panic!("only supports prefixes up to length 0xFF")
+    }
+    let mut out = Vec::with_capacity(prefix.len() + 1);
+    let length_bytes = (prefix.len() as u64).to_be_bytes();
+    out.extend_from_slice(&length_bytes[7..8]);
+    out.extend_from_slice(prefix);
+    out
+}
+
 pub struct PrefixedStorage<'a, T: Storage> {
     store: &'a mut T,
     prefix_impl: Vec<u8>,
@@ -12,7 +24,7 @@ where
     pub fn new(store: &'a mut T, prefix: &[u8]) -> Self {
         PrefixedStorage {
             store,
-            prefix_impl: [&[prefix.len() as u8], prefix].concat(),
+            prefix_impl: calculate_prefix_impl(prefix),
         }
     }
 }
@@ -41,6 +53,29 @@ where
 mod tests {
     use super::*;
     use cosmwasm::mock::MockStorage;
+
+    #[test]
+    fn calculate_prefix_impl_works() {
+        assert_eq!(calculate_prefix_impl(b""), b"\x00");
+        assert_eq!(calculate_prefix_impl(b"a"), b"\x01a");
+        assert_eq!(calculate_prefix_impl(b"ab"), b"\x02ab");
+        assert_eq!(calculate_prefix_impl(b"abc"), b"\x03abc");
+    }
+
+    #[test]
+    fn calculate_prefix_impl_works_for_long_prefix() {
+        let limit = 0xFF;
+        let long_prefix = vec![0; limit];
+        calculate_prefix_impl(&long_prefix);
+    }
+
+    #[test]
+    #[should_panic(expected = "only supports prefixes up to length 0xFF")]
+    fn calculate_prefix_impl_panics_for_too_long_prefix() {
+        let limit = 0xFF;
+        let long_prefix = vec![0; limit+1];
+        calculate_prefix_impl(&long_prefix);
+    }
 
     #[test]
     fn works() {
