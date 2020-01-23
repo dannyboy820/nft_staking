df485f9104f7ddbdd3ad378faf8523aadc4c5ef4
dannyboy820 <dannyboy.0193@gmail.com>
2020-01-22 21:07:52 -0800

    first draft of nameservice contract

---
+++ nameservice/src/msg.rs
@@ -0,0 +1,28 @@
+use cosmwasm::types::HumanAddr;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InitMsg {
+    pub name: String
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "lowercase")]
+pub enum HandleMsg {
+    Register { name: String },
+    Transfer { name: String, to: HumanAddr },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "lowercase")]
+pub enum QueryMsg {
+    // ResolveAddress returns the current address that the name resolves to
+    ResolveRecord { name: String },
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ResolveRecordResponse {
+    pub address: HumanAddr,
+}
