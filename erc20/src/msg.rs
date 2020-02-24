30c937dd893cb493ac97b6e73832ace02faba4ca
dannyboy820 <dannyboy.0193@gmail.com>
2020-02-24 15:28:06 +0100

    cargo fmt

--- erc20/src/msg.rs
+++ erc20/src/msg.rs
@@ -5,44 +5,44 @@ use cosmwasm::types::HumanAddr;
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema)]
 pub struct InitialBalance {
-  pub address: HumanAddr,
-  pub amount: String,
+    pub address: HumanAddr,
+    pub amount: String,
 }
 
 #[derive(Serialize, Deserialize, JsonSchema)]
 pub struct InitMsg {
-  pub name: String,
-  pub symbol: String,
-  pub decimals: u8,
-  pub initial_balances: Vec<InitialBalance>,
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub initial_balances: Vec<InitialBalance>,
 }
 
 #[derive(Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "lowercase")]
 pub enum HandleMsg {
-  Approve {
-    spender: HumanAddr,
-    amount: String,
-  },
-  Transfer {
-    recipient: HumanAddr,
-    amount: String,
-  },
-  TransferFrom {
-    owner: HumanAddr,
-    recipient: HumanAddr,
-    amount: String,
-  },
+    Approve {
+        spender: HumanAddr,
+        amount: String,
+    },
+    Transfer {
+        recipient: HumanAddr,
+        amount: String,
+    },
+    TransferFrom {
+        owner: HumanAddr,
+        recipient: HumanAddr,
+        amount: String,
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "lowercase")]
 pub enum QueryMsg {
-  Balance {
-    address: HumanAddr,
-  },
-  Allowance {
-    owner: HumanAddr,
-    spender: HumanAddr,
-  },
+    Balance {
+        address: HumanAddr,
+    },
+    Allowance {
+        owner: HumanAddr,
+        spender: HumanAddr,
+    },
 }
