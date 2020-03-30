a0156e2c70f2dca50931ec1da0909adc5e2db92a
dannyboy820 <dannyboy.0193@gmail.com>
2020-03-30 14:42:07 +0200

    Prepare optimizer command for all examples

--- build_test_all.sh
+++ build_test_all.sh
@@ -20,6 +20,13 @@ for example in ./*; do
         cargo wasm --locked
         cargo integration-test --locked
         cargo schema --locked
+
+        if [[ -n "$REGEN_OPT" ]]; then
+          docker run --rm -v "$(pwd)":/code \
+            --mount type=volume,source="$(basename "$(pwd)")"_cache,target=/code/target \
+            --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+            confio/cosmwasm-opt:0.7.3
+        fi
     )
   fi
 done
