15782102dd6d30c4a2f9932fcf339bdf1b027be7
dannyboy820 <dannyboy.0193@gmail.com>
2020-03-30 13:00:01 +0200

    Simplify lib.rs files

--- nameservice/src/lib.rs
+++ nameservice/src/lib.rs
@@ -3,12 +3,8 @@ pub mod contract;
 pub mod msg;
 pub mod state;
 
-/** Below we expose wasm exports * **/
-#[cfg(target_arch = "wasm32")]
-pub use cosmwasm::exports::{allocate, deallocate};
-
-#[cfg(target_arch = "wasm32")]
-pub use wasm::{handle, init};
+#[cfg(test)]
+mod tests;
 
 #[cfg(target_arch = "wasm32")]
 mod wasm {
@@ -17,7 +13,7 @@ mod wasm {
     use std::ffi::c_void;
 
     #[no_mangle]
-    pub extern "C" fn init(params_ptr: *mut c_void, msg_ptr: *mut c_void) -> *mut c_void {
+    extern "C" fn init(params_ptr: *mut c_void, msg_ptr: *mut c_void) -> *mut c_void {
         exports::do_init(
             &contract::init::<imports::ExternalStorage, imports::ExternalApi>,
             params_ptr,
@@ -26,7 +22,7 @@ mod wasm {
     }
 
     #[no_mangle]
-    pub extern "C" fn handle(params_ptr: *mut c_void, msg_ptr: *mut c_void) -> *mut c_void {
+    extern "C" fn handle(params_ptr: *mut c_void, msg_ptr: *mut c_void) -> *mut c_void {
         exports::do_handle(
             &contract::handle::<imports::ExternalStorage, imports::ExternalApi>,
             params_ptr,
@@ -35,13 +31,13 @@ mod wasm {
     }
 
     #[no_mangle]
-    pub extern "C" fn query(msg_ptr: *mut c_void) -> *mut c_void {
+    extern "C" fn query(msg_ptr: *mut c_void) -> *mut c_void {
         exports::do_query(
             &contract::query::<imports::ExternalStorage, imports::ExternalApi>,
             msg_ptr,
         )
     }
-}
 
-#[cfg(test)]
-mod tests;
+    // Other C externs like cosmwasm_api_0_6, allocate, deallocate are available
+    // automatically because we `use cosmwasm`.
+}
