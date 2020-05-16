89f8ace97566939d9760c80d14a78786f9b46b8e
dannyboy820 <dannyboy.0193@gmail.com>
2020-05-16 13:25:57 +0200

    Update escrow and make it compile

--- escrow/src/msg.rs
+++ escrow/src/msg.rs
@@ -1,7 +1,6 @@
+use cosmwasm_std::{Coin, HumanAddr};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-
-use cosmwasm::types::{Coin, HumanAddr};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
@@ -9,11 +8,11 @@ pub struct InitMsg {
     pub recipient: HumanAddr,
     /// When end height set and block height exceeds this value, the escrow is expired.
     /// Once an escrow is expired, it can be returned to the original funder (via "refund").
-    pub end_height: Option<i64>,
+    pub end_height: Option<u64>,
     /// When end time (in seconds since epoch 00:00:00 UTC on 1 January 1970) is set and
     /// block time exceeds this value, the escrow is expired.
     /// Once an escrow is expired, it can be returned to the original funder (via "refund").
-    pub end_time: Option<i64>,
+    pub end_time: Option<u64>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
