88a48644fbb3ebcd04d965162bca3fde38df3676
dannyboy820 <dannyboy.0193@gmail.com>
2020-08-15 14:00:04 +0200

    #83 Optimize contract and order import

--- voting/examples/schema.rs
+++ voting/examples/schema.rs
@@ -2,7 +2,7 @@ use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cw_voting::msg::{HandleMsg, InitMsg, QueryMsg, PollResponse, TokenStakeResponse};
+use cw_voting::msg::{HandleMsg, InitMsg, PollResponse, QueryMsg, TokenStakeResponse};
 use cw_voting::state::State;
 
 fn main() {
