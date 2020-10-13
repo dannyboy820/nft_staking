a7dcbdb3b36fff7626d7c6e277e1c1b9ee8d7929
dannyboy820 <dannyboy.0193@gmail.com>
2020-10-13 09:30:48 +0300

    Apply review recommendations

--- nameservice/src/error.rs
+++ nameservice/src/error.rs
@@ -24,6 +24,6 @@ pub enum ContractError {
     #[error("Name too long (length {length} min_length {max_length})")]
     NameTooLong { length: u64, max_length: u64 },
 
-    #[error("Invalid character( char {c}")]
+    #[error("Invalid character(char {c}")]
     InvalidCharacter { c: char },
 }
