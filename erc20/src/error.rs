2189a9aa9a1d585f9842bd1dcd0bc1a036f63a44
dannyboy820 <dannyboy.0193@gmail.com>
2020-10-14 15:28:17 +0200

    Clean up errors

--- erc20/src/error.rs
+++ erc20/src/error.rs
@@ -6,12 +6,6 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
-    #[error("unauthorized")]
-    Unauthorized {},
-
-    #[error("must reflect at least one message")]
-    NoReflectMsg {},
-
     #[error("Name is not in the expected format (3-30 UTF-8 bytes)")]
     NameWrongFormat {},
 
@@ -27,26 +21,6 @@ pub enum ContractError {
     #[error("Insufficient funds (balance {balance}, required={required})")]
     InsufficientFunds { balance: u128, required: u128 },
 
-    #[error("Corrupted data found 16 byte expected")]
+    #[error("Corrupted data found (16 byte expected)")]
     CorruptedDataFound {},
 }
-/*
-StdError::generic_err(
-            "",
-        )
-StdError::generic_err(
-            "Name is not in the expected format (3-30 UTF-8 bytes)",
-        )
-        StdError::generic_err(
-
-        )
-        StdError::generic_err()
-        StdError::generic_err(format!(
-            "Insufficient allowance: allowance={}, required={}",
-            allowance, amount_raw
-        ))
-        StdError::generic_err(format!(
-            "insufficient funds to burn: balance={}, required={}",
-            account_balance, amount_raw
-        )
- */
